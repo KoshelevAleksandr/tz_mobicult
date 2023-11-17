e8663c6999f8834da277fed3506bcfdacb48119f
Александр Кошелев <Koshelevsanya1994@yandex.ru>
2023-11-17 16:06:53 +0300

    create tests

--- tests/test_rates.py
+++ tests/test_rates.py
@@ -7,6 +7,8 @@ from api.models import rate
 from api.operations import init_currency_rate
 from conftest import SessionLocal_test
 
+client = TestClient(app)
+
 
 def test_init_rate():
     with SessionLocal_test() as session:
@@ -38,24 +40,23 @@ def test_update_rate():
 
 
 async def test_get_rates():
-    with TestClient(app) as client:
-        response = client.get("/", params={'day': 'today'})
-        assert response.status_code == 200, "Сегодня - неверный код ответа"
-        assert response.json() == [
-            {'id': 1, 'currency': 'USD', 'currency_value': 1.0, 'day': 'today'},
-            {'id': 2, 'currency': 'EUR', 'currency_value': 1.0, 'day': 'today'}
-        ], "Сегодня - неверный ответ"
+    response = client.get("/", params={'day': 'today'})
+    assert response.status_code == 200, "Сегодня - неверный код ответа"
+    assert response.json() == [
+        {'id': 1, 'currency': 'USD', 'currency_value': 1.0, 'day': 'today'},
+        {'id': 2, 'currency': 'EUR', 'currency_value': 1.0, 'day': 'today'}
+    ], "Сегодня - неверный ответ"
 
-        response = client.get("/", params={'day': 'yesterday'})
-        assert response.status_code == 200, "Вчера - неверный код ответа"
-        assert response.json() == [
-            {'id': 1, 'currency': 'USD', 'currency_value': 2.0, 'day': 'yesterday'},
-            {'id': 2, 'currency': 'EUR', 'currency_value': 2.0, 'day': 'yesterday'}
-        ], "Вчера - неверный ответ"
+    response = client.get("/", params={'day': 'yesterday'})
+    assert response.status_code == 200, "Вчера - неверный код ответа"
+    assert response.json() == [
+        {'id': 1, 'currency': 'USD', 'currency_value': 2.0, 'day': 'yesterday'},
+        {'id': 2, 'currency': 'EUR', 'currency_value': 2.0, 'day': 'yesterday'}
+    ], "Вчера - неверный ответ"
 
-        response = client.get("/", params={'day': 'before_yesterday'})
-        assert response.status_code == 200, "Позавчера - неверный код ответа"
-        assert response.json() == [
-            {'id': 1, 'currency': 'USD', 'currency_value': 3.0, 'day': 'before_yesterday'},
-            {'id': 2, 'currency': 'EUR', 'currency_value': 3.0, 'day': 'before_yesterday'}
-        ], "Позавчера - неверный ответ"
+    response = client.get("/", params={'day': 'before_yesterday'})
+    assert response.status_code == 200, "Позавчера - неверный код ответа"
+    assert response.json() == [
+        {'id': 1, 'currency': 'USD', 'currency_value': 3.0, 'day': 'before_yesterday'},
+        {'id': 2, 'currency': 'EUR', 'currency_value': 3.0, 'day': 'before_yesterday'}
+    ], "Позавчера - неверный ответ"
